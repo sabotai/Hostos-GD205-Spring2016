4c5fc905438df6f619597011d4f3056c425940b5
Alec McClure <sabotaione@gmail.com>
2016-02-08 17:25:53 -0500

    basic week 2 in class lesson
    featuring self-portrait and basic code manipulation

---
+++ Week2-BasicIntro/Whatever/Assets/Scripts/first.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class first : MonoBehaviour {
+
+	public GameObject textThing;
+
+	// Use this for initialization
+	void Start () {
+		textThing.GetComponent<TextMesh> ().text = "in quotation marks";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("space")) {
+			textThing.GetComponent<TextMesh> ().color = new Color (0.5f, 0.2f, 1f, 1f);
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			textThing.GetComponent<TextMesh> ().color = Color.cyan;
+		}
+		
+
+	}
+}
