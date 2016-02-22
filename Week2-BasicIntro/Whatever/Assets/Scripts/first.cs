c091bd35e39894b5e9861464d723aefcd8fd4a07
Alec McClure <sabotaione@gmail.com>
2016-02-22 16:17:14 -0500

    added week 3

--- Week2-BasicIntro/Whatever/Assets/Scripts/first.cs
+++ Week2-BasicIntro/Whatever/Assets/Scripts/first.cs
@@ -4,23 +4,41 @@ using System.Collections;
 public class first : MonoBehaviour {
 
 	public GameObject textThing;
+	int room;
+
+	bool hasKey;
 
 	// Use this for initialization
 	void Start () {
-		textThing.GetComponent<TextMesh> ().text = "in quotation marks";
+		room = 0;
+		hasKey = false;
+		textThing.GetComponent<TextMesh> ().text = "You are in room number " + room;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKeyDown ("space")) {
-			textThing.GetComponent<TextMesh> ().color = new Color (0.5f, 0.2f, 1f, 1f);
+		if (Input.GetKeyDown ("up")) {
+			Debug.Log ("Someone just pressed up.");
+			room++;
 		}
 
-		if (Input.GetKeyDown (KeyCode.A)) {
-			textThing.GetComponent<TextMesh> ().color = Color.cyan;
+		if (Input.GetKeyDown ("down")) {
+			if (room > 0) {
+				room--;
+			} else if (hasKey) {
+				room--;
+			}
 		}
-		
 
+		if (room == 2) {
+			hasKey = true;
+		}
+		textThing.GetComponent<TextMesh> ().text = "You are in room " + room;
+		//make another textThing (i.e. textThing2) tell you details about the room you are in
+		/*
+		if (room == 2) {
+			textThing2.GetComponent<TextMesh> ().text = "it smells weird in here";
+		}
+		*/
 	}
 }
---
+++ Week3-MoreText/Assets/Scripts/textStuff.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textStuff : MonoBehaviour {
+
+    //declare some new variables(containers) of various types
+    //making something "public" means you can change the assignment in the Unity editor
+	public GameObject myTextObject;
+	string upString;
+	string downString;
+	string roomName;
+
+	// Use this for initialization
+	void Start () {
+        //these are the default values we are starting our game with
+        //these happen to all be strings (strings of text)
+		roomName = "outside";
+		upString = "You look up at a starry night.";
+		downString = "You gaze down to see puddles lit by moonlight";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(roomName == "outside"){
+            //only do these things if the statement above is true
+
+            //if the player is outside, check these conditions
+            if (Input.GetKeyDown("up")){
+                //like... if the "up" key is pressed down, do something
+
+                myTextObject.GetComponent<TextMesh>().text = upString;
+                //this goes from general to specific, separated by periods
+                //we're telling it to change the value of something to whatever upString is
+                //in this case, that means myTextObject->TextMesh component->text value
+			}
+
+			if (Input.GetKeyDown("down")){
+				myTextObject.GetComponent<TextMesh>().text = downString;
+			}
+
+
+            //end of things to be done when roomName is equal to "outside"
+		} else{
+            //do something else if roomName is not "outside"... this part is optional
+        }
+		
+		if (Input.GetKeyDown("left")){
+            //if the key "left" is pressed down, do the following...
+
+            //first, change the roomName to "house"
+                //remember when we checked if the roomName was "outside"?
+                //now that will no longer apply, so we won't be checking for those earlier key presses
+			roomName = "house";
+
+            myTextObject.GetComponent<TextMesh>().text = "You are in a house.  The door is locked and now you're stuck :(";
+        
+            //that's the end of stuff to do when "left" is pressed down
+        }
+	}
+}
