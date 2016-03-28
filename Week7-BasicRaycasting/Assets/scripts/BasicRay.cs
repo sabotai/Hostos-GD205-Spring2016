2a8c88ebf0147e05efcd0ff01b70cd5a80f1aa31
Alec McClure <sabotaione@gmail.com>
2016-03-28 16:21:35 -0400

    added basic raycasting

---
+++ Week7-BasicRaycasting/Assets/scripts/BasicRay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicRay : MonoBehaviour {
+
+	public Transform obj;
+
+	// Update is called once per frame
+	void Update () {
+	
+		Ray myRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Camera.main.transform.LookAt (myRay.direction * 5);
+		Debug.DrawRay (myRay.origin, myRay.direction * 1000, Color.red);
+
+		RaycastHit rayhit = new RaycastHit ();
+
+		if (Physics.Raycast (myRay, out rayhit, 1000f) && Input.GetMouseButtonDown(0)) {
+			Debug.Log ("you done hit..." + rayhit.transform);
+			Instantiate (obj, rayhit.point, Random.rotation);
+			if (rayhit.transform.tag == "janeDoe") {
+				Debug.Log ("poor jane doe :(");
+			}
+		}	
+	}
+}
---
+++ Week7-BasicRaycasting/Assets/scripts/RaycastDemo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastDemo : MonoBehaviour {
+
+	public Transform blueprint; // assign in inspector
+	
+	// Update is called once per frame
+	void Update () {
+		// generate a ray based on camera position + mouse cursor screen coordinate
+		Ray ray = Camera.main.ScreenPointToRay ( Input.mousePosition );
+
+
+		Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+		Camera.main.transform.LookAt(ray.direction * 10);
+		// reserve space for info about where the raycast hit a thing, what it hit, etc.
+		RaycastHit rayHit = new RaycastHit(); // initialize forensics data container
+		
+		// actually shoot the raycast
+		if ( Physics.Raycast ( ray, out rayHit, 1000f ) && Input.GetMouseButtonDown (0) ) {
+			//transform.LookAt ( rayHit.point ); // make this object "look at" the raycast hit position
+			Instantiate ( blueprint, rayHit.point, Random.rotation ); // make a new clone at raycast hit position
+		}
+		
+	}
+}
